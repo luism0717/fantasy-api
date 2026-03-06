b9a3973c15906d62b461aae5a5b0660424dd3b4f
luism0717 <luism.0717@gmail.com>
2026-03-06 08:46:43 -0700

    add GET /players endpoint

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ app = FastAPI(
 @app.get("/")
 def root():
     return {"message": "Fantasy API is alive!"}
+
+@app.get("/players")
+def playerInfo():
+    return [
+        {"name": "Patrick Mahomes", "position": "QB", "nfl_team": "Kansas City Chiefs"},
+        {"name": "Justin Jefferson", "position": "WR", "nfl_team": "Minnesota Vikings"},
+        {"name": "Christian McCaffrey", "position": "RB", "nfl_team": "San Francisco 49ers"}
+    ]
